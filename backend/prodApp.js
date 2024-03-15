69d0bd07763bf623be6c602e5087f00f25096131
maluyacode <116046282+maluyacode@users.noreply.github.com>
2024-03-15 19:45:38 +0800

    prod files

---
+++ backend/prodApp.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const groupRoutes = require('./routes/groupRoutes');
+const announcementRoutes = require('./routes/announcementRoutes');
+const collabRoutes = require('./routes/collabRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
+const incidentRoutes = require('./routes/incidentRoutes');
+const videoTutorialRoutes = require('./routes/videoTutorialRoutes');
+
+const corsOptions = {
+    origin: 'https://tupt-online-platform.onrender.com', //included origin as true
+    credentials: true, //included credentials as true
+};
+
+app.use(cors(corsOptions));
+// app.options("*", cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/chat', chatRoutes);
+app.use('/api/v1/message', messageRoutes);
+app.use('/api/v1/group', groupRoutes);
+app.use('/api/v1/announcement', announcementRoutes);
+app.use('/api/v1/collab', collabRoutes);
+app.use('/api/v1/comment', commentRoutes);
+app.use('/api/v1/dashboard', dashboardRoutes);
+app.use('/api/v1/incident', incidentRoutes);
+app.use('/api/v1/video-tutorial', videoTutorialRoutes);
+
+
+module.exports = app;
---
+++ backend/production.js
@@ -0,0 +1,87 @@
+const dotenv = require('dotenv');
+const { Server } = require('socket.io');
+const mega = require('./utils/mega');
+const connectDatabase = require('./config/database');
+const app = require('./prodApp');
+
+dotenv.config({ path: './config/.env' });
+
+(async () => {
+    await mega.connect();
+})();
+require('./config/cloudinary');
+connectDatabase();
+
+const port = process.env.PORT || 8080;
+console.log(port)
+
+
+const server = app.listen(port, () => console.log(`Server Started`))
+
+
+const io = new Server(server, {
+    cors: {
+        origin: 'https://tupt-online-platform.onrender.com',
+        methods: ['GET', 'POST'],
+    },
+});
+
+const connectedUsers = new Map();
+
+io.on('connection', (socket) => {
+
+    // console.log(socket.handshake.query)
+    const { user } = socket.handshake.query
+    connectedUsers.set(user, socket);
+    // console.log(connectedUsers)
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+
+    socket.on('send', (data) => {
+        const { message, recipient } = JSON.parse(data);
+        console.log(recipient)
+        const user = connectedUsers.get(recipient);
+
+        socket.emit('recieved-message', recipient);
+        socket.broadcast.emit('push-to-admin', 'admin');
+        if (user) {
+            user.emit('recieved-message', recipient);
+        }
+
+    })
+
+    socket.on('hide-message', (data) => {
+        const { recipient } = JSON.parse(data);
+
+        const user = connectedUsers.get(recipient);
+
+        if (user) {
+            user.emit('show-hidden-message', recipient);
+        }
+    })
+
+    socket.on('new-announcement', (data) => {
+
+        const { teacher, announcement, group } = JSON.parse(data);
+        console.log(group)
+        if (group) {
+
+            const membersId = group.members.map(value => value._id);
+            connectedUsers.forEach((value, key) => {
+                if (membersId.includes(key)) {
+                    const user = connectedUsers.get(key);
+                    user.emit('push-announcement', data)
+                }
+            });
+
+        } else {
+
+            socket.broadcast.emit('push-announcement', data)
+
+        }
+
+    })
+
+});
