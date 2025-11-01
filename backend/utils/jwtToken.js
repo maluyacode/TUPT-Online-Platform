fe1eb6ec82536167e4b5524e62fd50e8fab64491
maluyacode <adlawandavemerc98@gmail.com>
2025-11-01 14:00:17 +0800

    samesite none

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -5,7 +5,8 @@ const sendToken = (user, statuscode, res, message = 'success') => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: false,
+        httpOnly: true,
+        sameSite: 'none',
     }
     res.status(statuscode).clearCookie('token').cookie('token', token, options).cookie('user', JSON.stringify(user), options).json({
         user,
